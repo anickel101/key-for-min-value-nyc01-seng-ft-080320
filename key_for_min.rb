f933c330efa90391cdeac4a3b60c8f3ce41478a2
Anson Nickel <anson.nickel@gmail.com>
2020-07-20 14:19:19 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,8 +4,8 @@
 def key_for_min_value(name_hash)
   min_value = 0
   name_hash.each do |key, value|
-    if name_hash[key] < min
-      min_value = name_hash[key]
+    if value < min_value
+      min_value = value
     end
   end
   name_hash.key[min_value]
