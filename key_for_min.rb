c031768e51390ed73c91c38b80bbabaa18b11b3c
Anson Nickel <anson.nickel@gmail.com>
2020-07-20 14:24:10 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  min_hash_key = name_
+  min_hash_key = nam
   min_value = name_hash[0]
   name_hash.each do |key, value|
     if value < min_value
